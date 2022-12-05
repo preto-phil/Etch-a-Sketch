3901480b97550369c22b385508360789091af881
Philip Pretorius <philippretorius.79@gmail.com>
2022-12-05 23:27:56 +0200

    yeah

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 
-//// function that creates/ appends squares
+//// function createDiv (sqr) {}
 
-    // append square inline-block
+    // var div = document.createElement('div');
+
+    // div.innerHTML = sqr 
+
+    // var container = document.createElementFragment();
 
 
 
 
+    // Styling properties and values
+        // document.getElementById ('  medDiv   ').style.display = 'inline-block';
+        // document.getElementById("myImg").style.height = "300px";
+        // document.getElementById("myImg").style.width = "300px";`
 
+        // return div.childNodes
 
 ////  function that creates x-amount of squares
 
@@ -15,9 +24,6 @@
 
         // call function that creates square
         // append square inline-block start
-
-
-
 
 //// Use set width and height throughout
 //// Size of square = height / number of squares && width / number of squares
