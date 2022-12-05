8117006a3af6dc54b0186341891e337e614c3923
Philip Pretorius <philippretorius.79@gmail.com>
2022-12-05 18:01:46 +0200

    Added files and folders, added html simple structure to be expanded, js pseudocode, css first steps

---
+++ script.js
@@ -0,0 +1,33 @@
+
+//// function that creates/ appends squares
+
+    // append square inline-block
+
+
+
+
+
+
+////  function that creates x-amount of squares
+
+    // Use loop to create squares
+    // for (i = x; i < x; i++)
+
+        // call function that creates square
+        // append square inline-block start
+
+
+
+
+//// Use set width and height throughout
+//// Size of square = height / number of squares && width / number of squares
+        // document.getElementById("myImg").style.height = "300px";
+        // document.getElementById("myImg").style.width = "300px";
+
+
+
+//// Interact with CSS ?
+
+        // Width 
+
+        // height
